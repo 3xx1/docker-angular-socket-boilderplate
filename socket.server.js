c1898511f61a3f8a43c7b83265bb50f0e4dc8c6e
Kaz Saegusa <chgm.bunbuker@gmail.com>
2018-11-01 21:21:38 -0700

    adding socket server

---
+++ socket.server.js
@@ -0,0 +1,28 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+// Opening Port at :3000
+http.listen(3000, function() {
+  console.log('[socket-server] Listening on :3000');
+});
+
+// Upon Connection
+io.on('connection', function(socket) {
+  // Client Connection Acq
+  console.log('[socket-server] A User Connected.');
+
+  // Updating States
+  socket.on('states.update', function(msg) {
+    io.emit('states.update', msg);
+  });
+
+  socket.on('action.dispatch', function(msg) {
+    socket.broadcast.emit('action.dispatch', msg);
+  });
+
+  // Refreshing All Screens
+  socket.on('refresh', function(msg) {
+    io.emit('refresh', true);
+  });
+});
